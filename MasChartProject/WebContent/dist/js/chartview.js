1c35ad4bfdfa685a610cfe16b45db602c7c04e2c
ggg008 <58506208+ggg008@users.noreply.github.com>
2020-05-13 20:07:54 +0900

    테마 변경 기능 추가
    0513:2007

--- MasChartProject/WebContent/dist/js/chartview.js
+++ MasChartProject/WebContent/dist/js/chartview.js
@@ -23,7 +23,7 @@
 	    this.container.style.background =
 	        'url(https://www.highcharts.com/samples/graphics/sand.png)';
 	});
-	Highcharts.theme = {
+	Highcharts.themeNormal = {
 	    colors: ['#f45b5b', '#8085e9', '#8d4654', '#7798BF', '#aaeeee',
 	        '#ff0066', '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
 	
@@ -68,7 +68,13 @@
 	            }
 	        }
 	    },
-	
+	    yAxis: {
+	        labels: {
+	            style: {
+	                color: '#6e6e70'
+	            }
+	        }
+	    },
 	    plotOptions: {
 	        series: {
 	            animation: {
@@ -84,28 +90,84 @@
 	        }
 	    },
 	    // Highstock specific
+
+	    navigation: {
+	        buttonOptions: {
+	            symbolStroke: '#666666',
+	            theme: {
+	                fill: 'white'
+	            }
+	        }
+	    },
 	    navigator: {
+	        handles: {
+	            backgroundColor: '#666',
+	            borderColor: '#AAA'
+	        },
+	        outlineColor: '#CCC',
+	        maskFill: 'rgba(255,255,255,0.1)',
 	        series: {
 	            color: '#343a40',
+	            lineColor: '#000000'
 	        },
 	        xAxis: {
 	            gridLineColor: '#D0D0D8'
 	        }
 	    },
+//	    rangeSelector: {
+//	        buttonTheme: {
+//	            fill: 'white',
+//	            stroke: '#C0C0C8',
+//	            'stroke-width': 1,
+//	            states: {
+//	                select: {
+//	                    fill: '#D0D0D8'
+//	                }
+//	            }
+//	        }
+//	    },
 	    rangeSelector: {
 	        buttonTheme: {
 	            fill: 'white',
 	            stroke: '#C0C0C8',
-	            'stroke-width': 1,
+	            style: {
+	                color: '#000'
+	            },
 	            states: {
+	                hover: {
+	                    fill: '#707073',
+	                    stroke: '#000000',
+	                    style: {
+	                        color: 'white'
+	                    }
+	                },
 	                select: {
-	                    fill: '#D0D0D8'
+	                    fill: '#d0d0d8',
+	                    stroke: '#000000',
+	                    style: {
+	                        color: 'black'
+	                    }
 	                }
 	            }
+	        },
+	        inputBoxBorderColor: '#505053',
+	        inputStyle: {
+	            backgroundColor: '#333',
+	            color: 'silver'
+	        },
+	        labelStyle: {
+	            color: 'silver'
 	        }
 	    },
 	    scrollbar: {
-	        trackBorderColor: '#C0C0C8'
+	        barBackgroundColor: '#808083',
+	        barBorderColor: '#808083',
+	        buttonArrowColor: '#CCC',
+	        buttonBackgroundColor: '#606063',
+	        buttonBorderColor: '#606063',
+	        rifleColor: '#FFF',
+	        trackBackgroundColor: '#404043',
+	        trackBorderColor: '#C0C0C8',	    
 	    },
 	
 	    
@@ -115,8 +177,214 @@
 	    },
 	};
 	
+	Highcharts.themeDark = {
+	    colors: ['#2b908f', '#90ee7e', '#f45b5b', '#7798BF', '#aaeeee', '#ff0066',
+	        '#eeaaee', '#55BF3B', '#DF5353', '#7798BF', '#aaeeee'],
+	    chart: {
+	        backgroundColor: {
+	            linearGradient: { x1: 0, y1: 0, x2: 1, y2: 1 },
+	            stops: [
+	                [0, '#2a2a2b'],
+	                [1, '#3e3e40']
+	            ]
+	        },
+	        style: {
+	            fontFamily: '\'Unica One\', sans-serif'
+	        },
+	        plotBorderColor: '#606063'
+	    },
+	    title: {
+	        style: {
+	            color: '#E0E0E3',
+	            //textTransform: 'uppercase',
+	            fontSize: '20px',
+	            fontWeight: 'bold'
+	        }
+	    },
+	    subtitle: {
+	        style: {
+	            color: '#E0E0E3',
+	            //textTransform: 'uppercase'
+	        }
+	    },
+	    xAxis: {
+	        //gridLineColor: '#707073',
+	        labels: {
+	            style: {
+	                color: '#E0E0E3'
+	            }
+	        },
+	        lineColor: '#707073',
+	        minorGridLineColor: '#505053',
+	        tickColor: '#707073',
+	        title: {
+	            style: {
+	                color: '#A0A0A3'
+	            }
+	        }
+	    },
+	    yAxis: {
+	        //gridLineColor: '#707073',
+	        labels: {
+	            style: {
+	                color: '#E0E0E3'
+	            }
+	        },
+	        lineColor: '#707073',
+	        minorGridLineColor: '#505053',
+//	        tickColor: '#707073',
+//	        tickWidth: 1,
+	        title: {
+	            style: {
+	                color: '#A0A0A3'
+	            }
+	        }
+	    },
+//	    tooltip: {
+//	        backgroundColor: 'rgba(0, 0, 0, 0.85)',
+//	        style: {
+//	            color: '#F0F0F0'
+//	        }
+//	    },
+	    plotOptions: {
+	        series: {
+	            dataLabels: {
+	                color: '#F0F0F3',
+	                style: {
+	                    fontSize: '13px'
+	                }
+	            },
+	            marker: {
+	                lineColor: '#333'
+	            }
+	        },
+	        boxplot: {
+	            fillColor: '#505053'
+	        },
+	        candlestick: {
+	            lineColor: 'white'
+	        },
+	        errorbar: {
+	            color: 'white'
+	        }
+	    },
+	    legend: {
+	        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+	        itemStyle: {
+	            color: '#E0E0E3'
+	        },
+	        itemHoverStyle: {
+	            color: '#FFF'
+	        },
+	        itemHiddenStyle: {
+	            color: '#606063'
+	        },
+	        title: {
+	            style: {
+	                color: '#C0C0C0'
+	            }
+	        }
+	    },
+	    credits: {
+	        style: {
+	            color: '#666'
+	        }
+	    },
+	    labels: {
+	        style: {
+	            color: '#707073'
+	        }
+	    },
+	    drilldown: {
+	        activeAxisLabelStyle: {
+	            color: '#F0F0F3'
+	        },
+	        activeDataLabelStyle: {
+	            color: '#F0F0F3'
+	        }
+	    },
+	    navigation: {
+	        buttonOptions: {
+	            symbolStroke: '#DDDDDD',
+	            theme: {
+	                fill: '#505053'
+	            }
+	        }
+	    },
+	    // scroll charts
+	    rangeSelector: {
+	        buttonTheme: {
+	            fill: '#505053',
+	            stroke: '#000000',
+	            style: {
+	                color: '#CCC'
+	            },
+	            states: {
+	                hover: {
+	                    fill: '#707073',
+	                    stroke: '#000000',
+	                    style: {
+	                        color: 'white'
+	                    }
+	                },
+	                select: {
+	                    fill: '#000003',
+	                    stroke: '#000000',
+	                    style: {
+	                        color: 'white'
+	                    }
+	                }
+	            }
+	        },
+	        inputBoxBorderColor: '#505053',
+	        inputStyle: {
+	            backgroundColor: '#333',
+	            color: 'silver'
+	        },
+	        labelStyle: {
+	            color: 'silver'
+	        }
+	    },
+	    navigator: {
+	        handles: {
+	            backgroundColor: '#666',
+	            borderColor: '#AAA'
+	        },
+	        outlineColor: '#CCC',
+	        maskFill: 'rgba(255,255,255,0.1)',
+	        series: {
+	            color: '#7798BF',
+	            lineColor: '#A6C7ED'
+	        },
+	        xAxis: {
+	            gridLineColor: '#505053'
+	        }
+	    },
+	    scrollbar: {
+	        barBackgroundColor: '#808083',
+	        barBorderColor: '#808083',
+	        buttonArrowColor: '#CCC',
+	        buttonBackgroundColor: '#606063',
+	        buttonBorderColor: '#606063',
+	        rifleColor: '#FFF',
+	        trackBackgroundColor: '#404043',
+	        trackBorderColor: '#404043'
+	    },
+
+	    loading: {
+	        hideDuration: 100,
+	        showDuration: 100
+	    },
+	};
+	
+	
+	
 	// Apply the theme
-	Highcharts.setOptions(Highcharts.theme);
+	//Highcharts.setOptions(Highcharts.themeNormal);
+
+
+	
+
 	
 	var cookies = document.cookie;
 	//console.log(cookies.length);
@@ -134,13 +402,41 @@
 	
 	cookieJson += '}';
 
-	//console.log(cookieJson);
+	console.log(cookieJson);
 	cookieJson = JSON.parse(cookieJson);	
 	
 	
 	var historyTime = cookieJson.historyTime != undefined ? cookieJson.historyTime : 'histominute';
 	var fromSymbol = cookieJson.fromSymbol != undefined ? cookieJson.fromSymbol : 'BTC';
 	var toSymbol = cookieJson.toSymbol != undefined ? cookieJson.toSymbol : 'USD';
+	var theme = cookieJson.theme != undefined ? cookieJson.theme : 'themeNormal';
+	
+	var setTheme = function(newTheme) {
+		
+		if(newTheme === 'themeNormal'){		
+			$(".page-wrapper").removeAttr("style");
+	    	$("#theme-point1").removeAttr("style");
+	    	// Apply the theme
+	    	Highcharts.setOptions(Highcharts.themeNormal);
+	    	var date = new Date();
+			date.setDate(date.getDate() + 30);//한달유지 
+			document.cookie = 'theme=themeNormal;expires=' + date.toString();
+			theme = 'themeNormal';
+		}
+		else if (newTheme === 'themeDark') {
+			
+			$(".page-wrapper").css("background", "#212223");
+			$("#theme-point1").css("background", "#191919");
+			// Apply the theme
+			Highcharts.setOptions(Highcharts.themeDark);
+			var date = new Date();
+			date.setDate(date.getDate() + 30);//한달유지 
+			document.cookie = 'theme=themeDark;expires=' + date.toString();
+			theme = 'themeDark';
+		}
+	}
+	
+	setTheme(theme);
 	
 	var chart = null;
 	var chart2 = null;
@@ -369,11 +665,6 @@
 	            	},
 	            },
 	    	    yAxis: [{
-	    	        labels: {
-	    	            style: {
-	    	                color: '#6e6e70'
-	    	            }
-	    	        },
 	                height: '80%',
 	    	        resize: {
 	    	            enabled: true
@@ -468,6 +759,7 @@
 			$('.highcharts-indicators').hide();
 			$('.highcharts-separator').eq(0).hide();
 			$('.highcharts-credits').hide();
+			$('.highcharts-toggle-toolbar').hide();
 			
 	        //			chart.rangeSelector.buttons.push(day);
 	        //			console.log(chart.rangeSelector);			
@@ -698,7 +990,5 @@
 		
 		
 	});
-
-
 	
 	
